aa784f29c5b5dc268e40e58dff446d2ce92350e4
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-06-15 17:16:49 -0300

    feat: shuffling the questions

--- src/pages/Game/components/Questions.jsx
+++ src/pages/Game/components/Questions.jsx
@@ -3,9 +3,20 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 function Questions({ questions }) {
-  const answers = [
-    ...questions[0].incorrect_answers, questions[0].correct_answer,
-  ];
+  // Math.random retorna números entre 0 e 1
+  const mathRandomMiddleNumber = 0.5;
+  const correctAnswer = {
+    question: questions[0].correct_answer,
+    dataTestid: 'correct-answer',
+  };
+  const incorrectAnswers = questions[0].incorrect_answers.map((incorrect, index) => ({
+    question: incorrect,
+    dataTestid: `wrong-answer-${index}`,
+  }));
+
+  let answers = [correctAnswer, ...incorrectAnswers];
+  // Shuffle nossa lista
+  answers = answers.sort(() => Math.random() - mathRandomMiddleNumber);
 
   return (
     <div>
@@ -14,8 +25,15 @@ function Questions({ questions }) {
         <p data-testid="question-text">{ questions[0].question }</p>
       </div>
       <div className="answers">
-        { answers.map((answer) => (
-          <button key={ answer } type="button">{ answer }</button>
+        { answers.map(({ question, dataTestid }) => (
+          <button
+            key={ question }
+            data-testid={ dataTestid }
+            type="button"
+          >
+            { question }
+
+          </button>
         )) }
       </div>
     </div>
