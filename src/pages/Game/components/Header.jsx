156bfd458e87ce61ebf36c870345d4576007aa9d
João Nasc <oi@joaonasc.dev>
2021-06-14 21:15:43 -0300

    feat: display real user data

--- src/pages/Game/components/Header.jsx
+++ src/pages/Game/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
   constructor() {
@@ -10,28 +12,34 @@ class Header extends React.Component {
     };
   }
 
-  componentDidMount() {
-    this.storeEmailHashToGravatar();
+  componentDidUpdate(prevProps) {
+    const { user } = this.props;
+    const userChanged = prevProps.user !== user;
+    if (userChanged) {
+      this.storeEmailHashToGravatar();
+    }
   }
 
   storeEmailHashToGravatar() {
-    const email = '';
+    const { user } = this.props;
 
     this.setState({
-      hashToGravatar: String(md5(email)),
+      hashToGravatar: String(md5(user.email)),
     });
   }
 
   render() {
     const { hashToGravatar } = this.state;
+    const { user } = this.props;
+    if (!user) return <h1>Carregando...</h1>;
     return (
       <div>
         <img
           src={ `https://www.gravatar.com/avatar/${hashToGravatar}` }
-          alt="Nome"
+          alt={ user.name }
           data-testid="header-profile-picture"
         />
-        <strong data-testid="header-player-name">Nome</strong>
+        <strong data-testid="header-player-name">{user.name}</strong>
         <span>
           Pontos:
           <span data-testid="header-score">0</span>
@@ -41,4 +49,15 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+const mapStateToProps = ({ settings: { user } }) => ({
+  user,
+});
+
+Header.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string,
+    name: PropTypes.string,
+  }),
+}.isRequired;
+
+export default connect(mapStateToProps)(Header);
