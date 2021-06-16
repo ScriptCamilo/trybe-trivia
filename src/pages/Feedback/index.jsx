46fc8173ca2ced23e3bdb759069184ac837006f7
Wendrick <wendrick.b.c1@gmail.com>
2021-06-16 17:05:11 -0300

    feat: Feedback text, score and total of questions

--- src/pages/Feedback/index.jsx
+++ src/pages/Feedback/index.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
 import Header from '../Game/components/Header';
+import { Link } from 'react-router-dom';
 
 class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  feedbackInfos() {
+    const minimunAssertions = 3;
+    return (
+      <section>
+        <p data-testid="feedback-text">
+          {0 < minimunAssertions ? 'Podia ser melhor...' : 'Mandou bem!'}
+        </p>
+        <p data-testid="feedback-total-score">
+          { `Pontuação Final: 0` }
+        </p>
+        <p data-testid="feedback-total-question">
+          { `Número de acertos: 0` }
+        </p>
+      </section>
+    )
+  }
+
   render() {
     return (
-      <Header />
+      <section>
+        <Header />
+        {this.feedbackInfos()}
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-play-again"
+          >
+            Jogar Novamente
+          </button>
+        </Link>
+        <Link to="/ranking">
+          <button
+            type="button"
+            data-testid="btn-ranking"
+          >
+            Ver Ranking
+          </button>
+        </Link>
+      </section>
     );
   }
 }
