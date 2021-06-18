ff16139f1559dce0656965d4a3041bc672a6a818
João Nasc <oi@joaonasc.dev>
2021-06-18 18:11:51 -0300

    feat(styles): header

--- src/pages/Game/components/Header/index.jsx
+++ src/pages/Game/components/Header/index.jsx
@@ -5,22 +5,29 @@ import { connect } from 'react-redux';
 
 import styles from './styles.module.css';
 
+import trophyIcon from '../../../../assets/icons/trophy.svg';
+
 function Header({ user, totalScore }) {
   const hash = String(md5(user.email));
 
   return (
     <div className={ styles.header }>
       <span className={ styles.user }>
-        <img
-          src={ `https://www.gravatar.com/avatar/${hash}` }
-          alt={ user.name }
-          data-testid="header-profile-picture"
-        />
+        <span className={ styles.userPhoto }>
+          <img
+            src={ `https://www.gravatar.com/avatar/${hash}` }
+            alt={ user.name }
+            data-testid="header-profile-picture"
+          />
+        </span>
         <strong data-testid="header-player-name">{user.name}</strong>
       </span>
       <span className={ styles.score }>
-        Pontos:
-        <span data-testid="header-score">{totalScore}</span>
+        <div>
+          Pontos:
+          <span data-testid="header-score">{totalScore}</span>
+        </div>
+        <img src={ trophyIcon } alt="Troféu" />
       </span>
     </div>
   );
