d35d637687c2b2e7302207af0c4b5346e8b7c15b
ScriptCamilo <script.camilo@gmail.com>
2022-09-14 20:29:09 -0300

    feat: deploy React app to GitHub Pages

---
+++ precache-manifest.913f45c20e23da79221d10b26493a2f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7ae4c0a9cf53e4a021867d790d9b46",
+    "url": "/trybe-trivia/index.html"
+  },
+  {
+    "revision": "aa8de50be66508aa1dc3",
+    "url": "/trybe-trivia/static/css/main.8b9c4a56.chunk.css"
+  },
+  {
+    "revision": "17a0dc7973af8c7bd379",
+    "url": "/trybe-trivia/static/js/2.2e150066.chunk.js"
+  },
+  {
+    "revision": "cb05d0d6781d1b0a4fbe5b6faf4600d7",
+    "url": "/trybe-trivia/static/js/2.2e150066.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa8de50be66508aa1dc3",
+    "url": "/trybe-trivia/static/js/main.f30a4573.chunk.js"
+  },
+  {
+    "revision": "bca91961c815181f131e",
+    "url": "/trybe-trivia/static/js/runtime-main.aebe4709.js"
+  },
+  {
+    "revision": "bacdab71d888cdfa19da1e028ff02a5d",
+    "url": "/trybe-trivia/static/media/trophy.bacdab71.svg"
+  }
+]);
